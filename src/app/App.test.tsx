b07d825690568b2a74eb1df6694ee61c71a43ba0
Stephen <sndegwa.n@outlook.com>
2020-10-30 19:08:54 +0300

    Setup

---
+++ src/app/App.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {shallow} from "enzyme";
+import App from './App';
+
+test('renders learn react link', () => {
+  let app = shallow(<App />);
+  expect(app.find(".App-header")).toBeTruthy();
+});
---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Projectful</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
